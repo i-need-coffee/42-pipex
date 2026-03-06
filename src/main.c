37122648b2133f7f4813aebd72362835672392dc
Shadya Jolliet <sjolliet@student.42.fr>
2026-03-06 14:13:13 +0100

    open write fd and check if there is an error

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sjolliet <sjolliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 16:16:53 by sjolliet          #+#    #+#             */
-/*   Updated: 2026/03/06 11:26:15 by sjolliet         ###   ########.fr       */
+/*   Updated: 2026/03/06 14:11:47 by sjolliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,12 @@ int	main(int argc, char **argv)
 	pipefds[0] = open(argv[1], O_RDONLY);
 	if (pipefds[0] == -1)
 		perror(argv[1]);
+	pipefds[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipefds[1] == -1)
+		perror(argv[4]);
 	if (pipefds[0] != -1)
 		close(pipefds[0]);
+	if (pipefds[1] != -1)
+		close(pipefds[1]);
 	return (0);
 }
